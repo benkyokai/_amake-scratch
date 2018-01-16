c69bbebd05ae648ed9c8f1a1c352f87c7e532a85
Aaron Madlon-Kay <aaron@madlon-kay.com>
2018-01-16 22:07:54 +0900

    End of part 3

---
+++ UFOGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public float rotation;
+
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+		rb2d.AddTorque (rotation);
+	}
+	
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2 (moveHorizontal * speed, moveVertical * speed);
+		rb2d.AddForce (movement);
+	}
+}
