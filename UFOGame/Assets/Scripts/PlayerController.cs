b499b5d39273e79fd531d48064141bd246a8d4e1
Aaron Madlon-Kay <aaron@madlon-kay.com>
2018-01-16 23:17:00 +0900

    End of part 7

--- UFOGame/Assets/Scripts/PlayerController.cs
+++ UFOGame/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
 	public float rotation;
 
+	public Text countText;
+	public Text winText;
+
 	private Rigidbody2D rb2d;
+
+	private int count;
 
 	// Use this for initialization
 	void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
 		rb2d.AddTorque (rotation);
+		count = 0;
+		SetCountText ();
+		winText.text = "";
 	}
 	
 	void FixedUpdate () {
@@ -25,6 +34,15 @@ public class PlayerController : MonoBehaviour {
 	void OnTriggerEnter2D (Collider2D other) {
 		if (other.gameObject.CompareTag ("PickUp")) {
 			other.gameObject.SetActive (false);
+			count++;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText () {
+		countText.text = "Count: " + count;
+		if (count >= 11) {
+			winText.text = "You Win!";
 		}
 	}
 }
