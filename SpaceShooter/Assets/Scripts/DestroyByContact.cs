78b0faf4a0b8b0b8b0f2c3b55e577cf9928a6d98
Aaron Madlon-Kay <aaron@madlon-kay.com>
2018-01-31 17:37:58 +0900

    Space Shooter: End of part 14

--- SpaceShooter/Assets/Scripts/DestroyByContact.cs
+++ SpaceShooter/Assets/Scripts/DestroyByContact.cs
@@ -6,15 +6,31 @@ public class DestroyByContact : MonoBehaviour {
 
 	public GameObject explosion;
 	public GameObject playerExplosion;
+	public int scoreValue;
+
+	private GameController gameController;
+
+	void Start() {
+		GameObject controllerObj = GameObject.FindWithTag ("GameController");
+		if (controllerObj != null) {
+			gameController = controllerObj.GetComponent<GameController> ();
+		}
+		if (gameController == null) {
+			Debug.Log ("Cannot find 'GameController' script");
+		}
+	}
 
 	void OnTriggerEnter(Collider other) {
-		if (!other.CompareTag ("Boundary")) {
-			Destroy (other.gameObject);
-			Destroy (gameObject);
-			Instantiate (explosion, transform.position, transform.rotation);
-			if (other.CompareTag ("Player")) {
-				Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
-			}
+		if (other.CompareTag ("Boundary")) {
+			return;
+		}
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+		Instantiate (explosion, transform.position, transform.rotation);
+		if (other.CompareTag ("Player")) {
+			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+		} else {
+			gameController.AddScore (scoreValue);
 		}
 	}
 }
