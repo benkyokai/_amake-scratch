34b1fa0db952060f3984572acc141408388e9efb
Aaron Madlon-Kay <aaron@madlon-kay.com>
2018-02-01 18:10:57 +0900

    Space Shooter: Partially implement enemy
    https://youtu.be/n67w7aEZKhg?t=1h08m00s

--- SpaceShooter/Assets/Scripts/DestroyByContact.cs
+++ SpaceShooter/Assets/Scripts/DestroyByContact.cs
@@ -21,12 +21,14 @@ public class DestroyByContact : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider other) {
-		if (other.CompareTag ("Boundary")) {
+		if (other.CompareTag ("Boundary") || other.CompareTag("Enemy")) {
 			return;
+		}
+		if (explosion != null) {
+			Instantiate (explosion, transform.position, transform.rotation);
 		}
 		Destroy (other.gameObject);
 		Destroy (gameObject);
-		Instantiate (explosion, transform.position, transform.rotation);
 		if (other.CompareTag ("Player")) {
 			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
 			gameController.GameOver ();
